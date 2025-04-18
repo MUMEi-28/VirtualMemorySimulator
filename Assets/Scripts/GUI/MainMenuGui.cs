47e29a4b2b510f7849de13adcda98957a7b1adfe
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-04-18 15:11:38 +0800

    MainMenu + about panel

---
+++ Assets/Scripts/GUI/MainMenuGui.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuGui : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Get called on exit button click
+    public void OnClickExit()
+    {
+        Application.Quit();
+        Debug.Log("Application Quit");
+    }
+
+    public void OnClickSounds()
+    {
+        Debug.Log("SOUND CLICKED");
+    }
+
+    public void OnClickGithub()
+    {
+        Application.OpenURL("https://github.com/MUMEi-28/VirtualMemorySimulator");
+    }
+}
