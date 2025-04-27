0ffaaa114dfdf1e5b3a930ff89b8a8acfea1d830
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-04-27 21:09:47 +0800

    sound bg

--- Assets/Scripts/GUI/MainMenuGui.cs
+++ Assets/Scripts/GUI/MainMenuGui.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenuGui : MonoBehaviour
 {
+	// SOUND BUTTON
+	public Image soundButton;
+	public Sprite soundButtonPressedBtnSprite;
+	private Sprite soundButtonUnpressedBtnSprite;
+	public AudioSource bgTrack;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        bgTrack = GameObject.FindGameObjectWithTag("BgTrack").GetComponent<AudioSource>();
+		soundButtonUnpressedBtnSprite = soundButton.sprite;
+	}
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
         
     }
@@ -26,8 +35,17 @@ public class MainMenuGui : MonoBehaviour
 
     public void OnClickSounds()
     {
-        Debug.Log("SOUND CLICKED");
-    }
+        if (bgTrack.isPlaying)
+        {
+            bgTrack.Pause();
+            soundButton.sprite = soundButtonPressedBtnSprite;
+        }
+        else
+        {
+            bgTrack.Play();
+            soundButton.sprite = soundButtonUnpressedBtnSprite;
+        }
+	}
 
     public void OnClickGithub()
     {
